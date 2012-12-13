fa96bf8d56ae9a19900264ea0c9ef214e4d5789f
bobtwinkles <srkoser+GitHub@gmail.com>
2012-12-13 17:57:46 -0500

    Converted 2D drawing to esplicit rectangle class, instead of random Pair<int, int>s.

---
+++ FPS/FPS/GLInterface/Rect2D.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Drawing;
+using OpenTK.Graphics.OpenGL;
+
+namespace FPS.GLInterface {
+	public class Rect2D {
+		int _vboid;
+		int _texid;
+
+		public Rect2D(Bitmap Tex, float X, float Y, float Width, float Height) {
+			_vboid = GLUtil.BuildRectangle(X, Y, Width, Height);
+			_texid = GLUtil.CreateTexture(Tex);
+		}
+
+		public void SetTexture(Bitmap Tex) {
+			GLUtil.UpdateTexture(Tex, _texid);
+		}
+
+		public void Render() {
+			GL.BindTexture(TextureTarget.Texture2D, _texid);
+			GL.BindBuffer(BufferTarget.ArrayBuffer, _vboid);
+			GL.InterleavedArrays(InterleavedArrayFormat.T2fC4fN3fV3f, 0, (IntPtr)0);
+			GL.DrawArrays(BeginMode.Quads, 0, 4);
+		}
+	}
+}
+
